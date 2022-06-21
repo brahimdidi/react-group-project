e029b75402719cd0cc53bed3e177b70907b0abf3
Talha <tkz9696@gmail.com>
2022-06-21 17:13:43 +0500

    Add header; fix linter errors

---
+++ src/Pages/Missions.js
@@ -0,0 +1,10 @@
+import Header from '../Components/Header';
+
+const Missions = () => (
+  <>
+    <Header />
+    <h1>Missions</h1>
+  </>
+);
+
+export default Missions;
---
+++ src/Pages/MyProfile.js
@@ -0,0 +1,10 @@
+import Header from '../Components/Header';
+
+const MyProfile = () => (
+  <>
+    <Header />
+    <h1>MyProfile</h1>
+  </>
+);
+
+export default MyProfile;
