bcd9960195e5347fcf17af5dfbe6073a7fabb904
Talha <tkz9696@gmail.com>
2022-06-23 02:15:17 +0500

    Add button and correct styling

--- src/Pages/Missions.js
+++ src/Pages/Missions.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Header from '../Components/Header';
 import { fetchMissions } from '../app/features/missionsReducer';
-import './Missions.module.css';
+import styles from './Missions.module.css';
 
 const Missions = () => {
   const dispatch = useDispatch();
@@ -15,22 +15,22 @@ const Missions = () => {
   return (
     <>
       <Header />
-      <h1>Missions</h1>
       <table>
         <thead>
           <tr>
             <td>Name</td>
             <td>Description</td>
             <td>Status</td>
+            <td />
           </tr>
         </thead>
         <tbody>
           {missions.map((missions) => (
             <tr key={missions.id}>
-              <td>{missions.mission_name}</td>
+              <td className={styles.mission_name}>{missions.mission_name}</td>
               <td>{missions.description}</td>
               <td>NOT A MEMBER</td>
-              <td>Join Mission</td>
+              <td><button type="button">Join Mission</button></td>
             </tr>
           ))}
         </tbody>
