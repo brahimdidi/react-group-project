3e2c463cc3338b0561816336f8164e479b17d3bc
brahimgb1team@gmail.com <brahimgb1team@gmail.com>
2022-06-22 09:55:06 +0000

    create store with configureStore import rocketreducer export store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,11 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from '@reduxjs/toolkit';
+import rocketsReducer from './features/rocketsReducer';
 
-// export const store = configureStore({
-//   reducer: {
-//     counter: counterReducer,
-//   },
-// });
+// import missionsReducer from './features/missionsReducer';
+
+const store = configureStore({
+  reducer: {
+    rockets: rocketsReducer,
+  },
+});
+export default store;
