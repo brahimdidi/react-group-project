11e1099422f1e160cffb93c90726e78996bef817
brahimgb1team@gmail.com <brahimgb1team@gmail.com>
2022-06-22 09:56:42 +0000

    import rockets list use selector to dispatch list,render list,useEffect fetch once only

--- src/Pages/Rockets.js
+++ src/Pages/Rockets.js
@@ -1,10 +1,38 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+// import { selectRockets } from '../app/features/rocketsReducer';
 import Header from '../Components/Header';
+import { fetchRockets, selectRockets, selectStatus } from '../app/features/rocketsReducer';
 
-const Rockets = () => (
-  <>
-    <Header />
-    <h1>Rockets</h1>
-  </>
-);
+const Rockets = () => {
+  const rocketsList = useSelector(selectRockets);
+  const listStatus = useSelector(selectStatus);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]);
+  return (
+    <div>
+      <Header />
+      <h1>
+        The rockets wer fetched with
+        {' '}
+        {listStatus}
+      </h1>
+      <ul>
+        { rocketsList.map((rocket) => (
+          <li key={rocket.id}>
+            id:
+            {rocket.id}
+            {' '}
+            name:
+            {rocket.rocket_name}
+          </li>
+        ))}
+      </ul>
+
+    </div>
+  );
+};
 
 export default Rockets;
