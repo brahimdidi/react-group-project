29a2840480b90810dc5296b7b02c9e9ae132cdd6
brahimgb1team@gmail.com <brahimgb1team@gmail.com>
2022-06-22 17:08:38 +0000

    toggle styles and text dispatch reserve action

--- src/Components/ReserveButton.js
+++ src/Components/ReserveButton.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { reserveRocket, selectState } from '../app/features/rocketsReducer';
 
 const ReserveButton = (props) => {
-  const { reservedValue } = props;
+  const currentState = useSelector(selectState);
+  const dispatch = useDispatch();
+  const { reservedValue, btnText } = props;
+
   const reservedStyle = {
-    color: '#595959',
-    opacity: 0.4,
-    textDecoration: 'line-through',
+    background: 'hsl(0deg 0% 100%)',
+    color: 'hsl(204deg 6% 65%)',
+    width: '180px',
+    border: '1px solid hsl(210deg 6% 81%)',
   };
-  return (<button style={reservedValue ? reservedStyle : null}>reserve </button>);
+  const nonReservedStyle = {
+    background: 'hsl(211deg 100% 50%)',
+    color: 'white',
+    width: '150px',
+    border: 'unset',
+    padding: '1px',
+  };
+  const btnSetStyle = (e) => (e === 'true' ? reservedStyle : nonReservedStyle);
+
+  return (
+    <button
+      onClick={() => dispatch(reserveRocket(currentState))}
+      style={btnSetStyle(reservedValue)}
+    >
+      {btnText(reservedValue)}
+    </button>
+  );
 };
 
 export default ReserveButton;
