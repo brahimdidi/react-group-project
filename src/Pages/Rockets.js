222ba75d9a1a97cc7e3ceda9d21be5dd9e328ed9
brahimgb1team@gmail.com <brahimgb1team@gmail.com>
2022-06-22 21:04:06 +0000

    get reservedValue to get it directly from the rocket item.

--- src/Pages/Rockets.js
+++ src/Pages/Rockets.js
@@ -2,13 +2,11 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import Header from '../Components/Header';
 import ReserveButton from '../Components/ReserveButton';
-import { fetchRockets, selectRockets, selectReserved } from '../app/features/rocketsReducer';
-import store from '../app/store';
+import { fetchRockets, selectRockets } from '../app/features/rocketsReducer';
 
 const Rockets = () => {
   // define state keys
   const rocketsList = useSelector(selectRockets);
-  const reservedValue = useSelector(selectReserved);
   // function to toggle reserve button text
   const notreserved = 'reserve';
   const reserved = 'cancel reservation';
@@ -40,17 +38,21 @@ const Rockets = () => {
       <ul className="rockets-container">
         { rocketsList.map((rocket) => (
           <li className="rocket-card" key={rocket.id} id={rocket.id}>
-            <img className="rocket-img" src={rocket.flickr_images[1]} alt="rocket" />
+            <img className="rocket-img" src={rocket.img[1]} alt="rocket" />
             <div className="rocket-infos-div">
               <h1>{rocket.rocket_name}</h1>
               <p>
-                <span style={spanSetStyle(reservedValue)}>
+                <span style={spanSetStyle(rocket.reserved)}>
                   Reserved
                 </span>
                 {' '}
                 {rocket.description}
               </p>
-              <ReserveButton reservedValue={reservedValue} btnText={btnText} />
+              <ReserveButton
+                reservedValue={rocket.reserved}
+                btnText={btnText}
+                rocketId={rocket.id}
+              />
             </div>
           </li>
         ))}
