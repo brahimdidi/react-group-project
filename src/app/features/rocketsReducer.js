76bc530f37eaea9dbecdf1e693905318faa60b01
brahimgb1team@gmail.com <brahimgb1team@gmail.com>
2022-06-22 17:07:50 +0000

    add reservedRocket action

--- src/app/features/rocketsReducer.js
+++ src/app/features/rocketsReducer.js
@@ -2,14 +2,22 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const rocketsUrl = 'https://api.spacexdata.com/v3/rockets';
 
-export const fetchRockets = createAsyncThunk('rockets/getRockets', async () => fetch(rocketsUrl).then((res) => res.json()));
+export const fetchRockets = createAsyncThunk('rockets/getRockets',
+  async () => fetch(rocketsUrl).then((res) => res.json()));
 
 const rocketsSlice = createSlice({
   name: 'rockets',
   initialState: {
     list: [],
     status: null,
-    reserved: true,
+    reserved: false,
+  },
+  reducers: {
+    reserveRocket: (state) => {
+      const newReserved = state.reserved;
+      const setReserved = (e) => (e === 'true' ? 'false' : 'true');
+      return { ...state, reserved: setReserved(newReserved) };
+    },
   },
   extraReducers: {
     [fetchRockets.pending]: (state) => {
@@ -25,7 +33,10 @@ const rocketsSlice = createSlice({
     },
   },
 });
+
+export const { reserveRocket } = rocketsSlice.actions;
 export const selectRockets = (state) => state.rockets.list;
 export const selectStatus = (state) => state.rockets.status;
 export const selectReserved = (state) => state.rockets.reserved;
+export const selectState = (state) => state.rockets;
 export default rocketsSlice.reducer;
