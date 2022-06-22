a28fb65771ed7f2dd5cdbab54edc270c7514cafd
brahimgb1team@gmail.com <brahimgb1team@gmail.com>
2022-06-22 21:00:13 +0000

    edit fetch action to map the rocketsList add resrved to each el

--- src/app/features/rocketsReducer.js
+++ src/app/features/rocketsReducer.js
@@ -3,20 +3,37 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 const rocketsUrl = 'https://api.spacexdata.com/v3/rockets';
 
 export const fetchRockets = createAsyncThunk('rockets/getRockets',
-  async () => fetch(rocketsUrl).then((res) => res.json()));
+  async () => {
+    const res = await fetch(rocketsUrl);
+    const rockets = await res.json();
+    const rocketsList = rockets.map((rocket) => ({
+      id: rocket.rocket_id,
+      rocket_name: rocket.rocket_name,
+      description: rocket.description,
+      img: rocket.flickr_images,
+      reserved: false,
+    }));
+    return rocketsList;
+  });
 
 const rocketsSlice = createSlice({
   name: 'rockets',
   initialState: {
     list: [],
-    status: null,
-    reserved: false,
+
   },
   reducers: {
-    reserveRocket: (state) => {
-      const newReserved = state.reserved;
+    reserveRocket: (state, action) => {
+      const { list, id } = action.payload;
       const setReserved = (e) => (e === 'true' ? 'false' : 'true');
-      return { ...state, reserved: setReserved(newReserved) };
+      const newList = list.map((el) => {
+        switch (el.id) {
+          case (id):
+            return { ...el, reserved: setReserved(el.reserved) };
+          default: return el;
+        }
+      });
+      state.list = newList;
     },
   },
   extraReducers: {
@@ -28,7 +45,6 @@ const rocketsSlice = createSlice({
       state.status = 'success';
     },
     [fetchRockets.rejected]: (state) => {
-      state.list = payload;
       state.status = 'failed';
     },
   },
@@ -36,7 +52,5 @@ const rocketsSlice = createSlice({
 
 export const { reserveRocket } = rocketsSlice.actions;
 export const selectRockets = (state) => state.rockets.list;
-export const selectStatus = (state) => state.rockets.status;
-export const selectReserved = (state) => state.rockets.reserved;
 export const selectState = (state) => state.rockets;
 export default rocketsSlice.reducer;
