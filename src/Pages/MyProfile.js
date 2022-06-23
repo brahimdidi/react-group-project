d69100df20f125ab250fe37e603d92a2bab36f24
brahimgb1team@gmail.com <brahimgb1team@gmail.com>
2022-06-23 17:34:50 +0000

    grab data ,filter data to keep reserved,render the UI depends on the state,add logic to cancel reservation

--- src/Pages/MyProfile.js
+++ src/Pages/MyProfile.js
@@ -1,31 +1,47 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Header from '../Components/Header';
 import styles from './MyProfile.module.css';
-import { selectRockets } from '../app/features/rocketsReducer';
+import { selectRockets, reserveRocket } from '../app/features/rocketsReducer';
 
 const MyProfile = () => {
   const rocketsList = useSelector(selectRockets);
   const reservedRockets = rocketsList.filter((b) => b.reserved);
+  const reservedMissions = [{ name: 'new', id: '23', reserved: 'true' },
+    { name: 'any', id: '20', reserved: 'true' }, { name: 'mis', id: '22', reserved: 'true' }];
   const generateKey = () => `_${Math.random().toString(36).substr(2, 9)}`;
+  const dispatch = useDispatch();
+  const clickHandler = (list, id) => {
+    dispatch(list(id));
+  };
+  // JSX items
+  const itemJsx = (e) => (
+    <li key={generateKey()} className={styles.myItem}>
+      <h1 className={styles.myItemName}>{e.name}</h1>
+      <button type="button" onClick={() => clickHandler(reserveRocket, e.id)}>Cancel Reservation</button>
+    </li>
+  );
+  const profileEmpty = (b) => (
+    <li key={generateKey()} className={styles.noItems}>
+      You have no
+      {b}
+      {' '}
+      yet!
+    </li>
+  );
   return (
     <div>
       <Header />
       <section className={styles.myProfileSection}>
-        <div className={styles.myProfileMissions}>
+        <div className={styles.myProfileItem}>
           <h1>My Missions</h1>
-          <ul className={styles.myProfileMissionsList}>
-            <li key={generateKey()}>bamo</li>
-            <li>an</li>
-            <li>di</li>
-            <li>di</li>
+          <ul className={styles.myProfileItemList}>
+            {reservedMissions.length ? ((reservedMissions.map((mission) => (itemJsx(mission))))) : profileEmpty('Missions reserved') }
           </ul>
         </div>
-        <div className={styles.myProfileRockets}>
-          <h1>My Rockets</h1>
-          <ul className={styles.myProfileRocketsList}>
-            { reservedRockets.map((rocket) => (
-              <li key={generateKey()}>{rocket.rocket_name}</li>
-            ))}
+        <div className={styles.myProfileItem}>
+          <h1 className={styles.myItemName}>My Rockets</h1>
+          <ul className={styles.myProfileItemList}>
+            {reservedRockets.length ? ((reservedRockets.map((rocket) => (itemJsx(rocket))))) : profileEmpty('Rockets reserved') }
           </ul>
         </div>
       </section>
