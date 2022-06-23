304f6af06784b5cea142dd45f01ef712951753f9
brahimgb1team@gmail.com <brahimgb1team@gmail.com>
2022-06-23 14:53:38 +0000

    comment out unsed file

--- src/Components/ReserveButton.js
+++ src/Components/ReserveButton.js
@@ -1,35 +1,24 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { reserveRocket, selectState } from '../app/features/rocketsReducer';
+// import React from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { reserveRocket, selectState } from '../app/features/rocketsReducer';
 
-const ReserveButton = (props) => {
-  const currentState = useSelector(selectState);
-  const dispatch = useDispatch();
-  const { reservedValue, btnText, rocketId } = props;
-  const reservedStyle = {
-    background: 'hsl(0deg 0% 100%)',
-    color: 'hsl(204deg 6% 65%)',
-    width: '180px',
-    border: '1px solid hsl(210deg 6% 81%)',
-  };
-  const nonReservedStyle = {
-    background: 'hsl(211deg 100% 50%)',
-    color: 'white',
-    width: '150px',
-    border: 'unset',
-    padding: '1px',
-  };
-  const btnSetStyle = (e) => (e === 'true' ? reservedStyle : nonReservedStyle);
+// const ReserveButton = (props) => {
+//   // Grab the current state
+//   const currentState = useSelector(selectState);
+//   const dispatch = useDispatch();
+//   // Use desructuring to get values passed by parent Element
+//   const { btnText, rocketId, className } = props;
 
-  return (
-    <button
-      type="button"
-      onClick={() => dispatch(reserveRocket({ list: currentState.list, id: rocketId }))}
-      style={btnSetStyle(reservedValue)}
-    >
-      {btnText(reservedValue)}
-    </button>
-  );
-};
+//   // Render this Button on the UI
+//   return (
+//     <button
+//       type="button"
+//       onClick={() => dispatch(reserveRocket({ list: currentState.list, id: rocketId }))}
+//       className={className}
+//     >
+//       {btnText}
+//     </button>
+//   );
+// };
 
-export default ReserveButton;
+// export default ReserveButton;
