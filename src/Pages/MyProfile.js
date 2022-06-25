b7256882dd402e4e1af58642035771738293b6c7
brahimgb1team@gmail.com <brahimgb1team@gmail.com>
2022-06-25 16:26:48 +0000

    render missions use the state to filter the array show joined missions

--- src/Pages/MyProfile.js
+++ src/Pages/MyProfile.js
@@ -2,27 +2,36 @@ import { useSelector, useDispatch } from 'react-redux';
 import Header from '../Components/Header';
 import styles from './MyProfile.module.css';
 import { selectRockets, reserveRocket } from '../app/features/rocketsReducer';
+import { selectMissions,joinMission } from '../app/features/missionsReducer';
 
 const MyProfile = () => {
   const rocketsList = useSelector(selectRockets);
+  const missionsList = useSelector(selectMissions);
   const reservedRockets = rocketsList.filter((b) => b.reserved);
-  const reservedMissions = [{ name: 'new', id: '23', reserved: 'true' },
-    { name: 'any', id: '20', reserved: 'true' }, { name: 'mis', id: '22', reserved: 'true' }];
+  const reservedMissions = missionsList.filter((b) => b.joined);
   const generateKey = () => `_${Math.random().toString(36).substr(2, 9)}`;
   const dispatch = useDispatch();
   const clickHandler = (list, id) => {
     dispatch(list(id));
   };
   // JSX items
-  const itemJsx = (e) => (
+  // function to shorten name
+  const shotName = (name) => {
+    if (name.length > 25) {
+      name = name.substring(0, 24) + "...";
+    }
+    return name;
+  };
+  const itemJsx = (e, i, btnText) => (
     <li key={generateKey()} className={styles.myItem}>
-      <h1 className={styles.myItemName}>{e.name}</h1>
-      <button type="button" onClick={() => clickHandler(reserveRocket, e.id)}>Cancel Reservation</button>
+      <h1 className={styles.myItemName}>{shotName(e.name)}</h1>
+      <button type="button" onClick={() => clickHandler(i, e.id)}>{btnText}</button>
     </li>
   );
   const profileEmpty = (b) => (
     <li key={generateKey()} className={styles.noItems}>
       You have no
+      {' '}
       {b}
       {' '}
       yet!
@@ -35,13 +44,13 @@ const MyProfile = () => {
         <div className={styles.myProfileItem}>
           <h1>My Missions</h1>
           <ul className={styles.myProfileItemList}>
-            {reservedMissions.length ? ((reservedMissions.map((mission) => (itemJsx(mission))))) : profileEmpty('Missions reserved') }
+            {reservedMissions.length ? ((reservedMissions.map((mission) => (itemJsx(mission, joinMission, 'leave mission'))))) : profileEmpty('Missions reserved') }
           </ul>
         </div>
         <div className={styles.myProfileItem}>
           <h1>My Rockets</h1>
           <ul className={styles.myProfileItemList}>
-            {reservedRockets.length ? ((reservedRockets.map((rocket) => (itemJsx(rocket))))) : profileEmpty('Rockets reserved') }
+            {reservedRockets.length ? ((reservedRockets.map((rocket) => (itemJsx(rocket, reserveRocket, 'cancel reservation'))))) : profileEmpty('Rockets reserved') }
           </ul>
         </div>
       </section>
