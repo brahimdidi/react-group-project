1f9d16109290645ee0bf811f81b1498b19af4a88
brahimgb1team@gmail.com <brahimgb1team@gmail.com>
2022-06-25 16:29:56 +0000

    fix linter issue with reassigning

--- src/Pages/MyProfile.js
+++ src/Pages/MyProfile.js
@@ -2,7 +2,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import Header from '../Components/Header';
 import styles from './MyProfile.module.css';
 import { selectRockets, reserveRocket } from '../app/features/rocketsReducer';
-import { selectMissions,joinMission } from '../app/features/missionsReducer';
+import { selectMissions, joinMission } from '../app/features/missionsReducer';
 
 const MyProfile = () => {
   const rocketsList = useSelector(selectRockets);
@@ -15,16 +15,17 @@ const MyProfile = () => {
     dispatch(list(id));
   };
   // JSX items
-  // function to shorten name
-  const shotName = (name) => {
+  // function to checken name
+  const checkName = (name) => {
     if (name.length > 25) {
-      name = name.substring(0, 24) + "...";
+      const newName = `${name.substring(0, 24)}...`;
+      return newName;
     }
     return name;
   };
   const itemJsx = (e, i, btnText) => (
     <li key={generateKey()} className={styles.myItem}>
-      <h1 className={styles.myItemName}>{shotName(e.name)}</h1>
+      <h1 className={styles.myItemName}>{checkName(e.name)}</h1>
       <button type="button" onClick={() => clickHandler(i, e.id)}>{btnText}</button>
     </li>
   );
